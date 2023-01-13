979bc60786e6532460748168216c15437ce7a539
Beyza Uyanıksoy <beyzauyanik57@hotmail.com>
2023-01-13 15:04:41 +0300

    routes da degişiklik yapıldı

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,9 +4,9 @@ abstract class Routes {
   static const LOGIN = '/login';
   static const DASHBOARD = '/dashboard';
 
-    static const BORDRODETAIL = '/bordro_detail';
-    static const BODRO = '/bodro';
-    static const VACATION = '/vacation';
-    static const WORKS = '/works';
+  static const BORDRODETAIL = '/bordro_detail';
+  static const BODRO = '/bodro';
+  static const VACATION = '/vacation';
+  static const WORKS = '/works';
   static const MYREQUEST = '/my_request';
 }
